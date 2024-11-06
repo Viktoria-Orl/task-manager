987fbbdde337508bd950370ffdc00109d2056171
Victoria Orleanova <Vika9_96@mail.ru>
2024-11-06 20:41:33 +0300

    refactor: apply prettier

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let dateHeading = document.getElementById('dateHeading');
+let dateHeading = document.getElementById("dateHeading");
 
 let date = new Date();
 
 const monthName = {
-    0: 'january',
-    1: 'february',
-    2: 'march',
-    3: 'april',
-    4: 'may',
-    5: 'june',
-    6: 'july',
-    7: 'august',
-    8: 'september',
-    9: 'october',
-    10: 'november',
-    11: 'december',
+  0: "january",
+  1: "february",
+  2: "march",
+  3: "april",
+  4: "may",
+  5: "june",
+  6: "july",
+  7: "august",
+  8: "september",
+  9: "october",
+  10: "november",
+  11: "december",
 };
 
-dateHeading.innerText += `Plan for ${date.getDate()} ${monthName[date.getMonth()]}`;
+dateHeading.innerText += `Plan for ${date.getDate()} ${
+  monthName[date.getMonth()]
+}`;
 
 let list;
 
@@ -30,44 +32,52 @@ try {
 
 const taskWidget = document.querySelector(".taskWidget");
 
-const tasksList = document.createElement('ul');
+const tasksList = document.createElement("ul");
 
 function renderList() {
-  tasksList.innerHTML = '';
+  tasksList.innerHTML = "";
 
   list.forEach((element) => {
     tasksList.insertAdjacentHTML(
       "beforeend",
       `
         <li class="task">
-          <label class="taskName ${element.completed ? 'completed' : ''}">
-            <input type="checkbox" class="checkboxInput" ${element.completed ? 'checked' : ''}>
+          <label class="taskName ${element.completed ? "completed" : ""}">
+            <input type="checkbox" class="checkboxInput" ${
+              element.completed ? "checked" : ""
+            }>
           ${element.taskName}
           </label>
           <button class="buttonDeleteTask" id="${element.id}">🗑️</button>
         </li>
-      `)
+      `
+    );
   });
 
   //обработчик удаления на каждую вновь созданную кнопку delete
 
-  tasksList.querySelectorAll('.buttonDeleteTask').forEach(button => {
-    button.addEventListener('click', deleteItem);
+  tasksList.querySelectorAll(".buttonDeleteTask").forEach((button) => {
+    button.addEventListener("click", deleteItem);
   });
 
   // обработчик изменения статуса выполнения задачи
 
-  tasksList.querySelectorAll('.checkboxInput').forEach(element => element.addEventListener('change', completedToggle));
+  tasksList
+    .querySelectorAll(".checkboxInput")
+    .forEach((element) => element.addEventListener("change", completedToggle));
 
   // cчетчики для виджета выполненных задач
 
   const countTasks = document.getElementsByClassName("taskName").length;
 
-  const countCompletedTasks = document.getElementsByClassName("completed").length;
+  const countCompletedTasks =
+    document.getElementsByClassName("completed").length;
 
   // актуализация виджета
 
-  taskWidget.innerText = `${countCompletedTasks}/${countTasks} ${countCompletedTasks > 1 ? "tasks" : "task"} completed today`;
+  taskWidget.innerText = `${countCompletedTasks}/${countTasks} ${
+    countCompletedTasks > 1 ? "tasks" : "task"
+  } completed today`;
 
   localStorage.setItem("list", JSON.stringify(list));
 }
@@ -78,29 +88,28 @@ dateHeading.after(tasksList);
 
 //добавление задач
 
-const inputAddTask = document.querySelector('.inputAddTask');
-const buttonAddTask = document.querySelector('.buttonAddTask');
+const inputAddTask = document.querySelector(".inputAddTask");
+const buttonAddTask = document.querySelector(".buttonAddTask");
 
 function addItem() {
-
   const newId = list.length ? list[list.length - 1].id + 1 : 1;
   if (inputAddTask.value) {
-    list.push(
-      {taskName: inputAddTask.value, 
-      completed: false, 
-      id: newId}
-    );
-    inputAddTask.value = '';
+    list.push({ id: newId, taskName: inputAddTask.value, completed: false });
+    inputAddTask.value = "";
     renderList();
-  } else {alert('Empty task!');}
+  } else {
+    alert("Empty task!");
+  }
 }
 
-buttonAddTask.addEventListener('click', addItem);
+buttonAddTask.addEventListener("click", addItem);
 
 //удаление задач
 
 function deleteItem(event) {
-  const idDeletedTask = list.findIndex((task) => task.id === Number(event.target.id));
+  const idDeletedTask = list.findIndex(
+    (task) => task.id === Number(event.target.id)
+  );
   list.splice(idDeletedTask, 1);
 
   renderList();
